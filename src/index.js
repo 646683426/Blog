f1f1ff6ccd3f0855fa75f65b16a309c8d00946c0
曹鑫 <646683426@qq.com>
2020-12-29 14:02:12 +0800

    路由组件

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './component/App';
-import List from './component/list';
-import Detail from './component/detail'
+import App from './pages/App';
+import List from './pages/list';
+import Detail from './pages/detail'
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router,Route} from 'react-router-dom'
 ReactDOM.render(
