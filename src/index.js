e3375a1b2f396dcb11c21d5c85196819615f8abb
曹鑫 <646683426@qq.com>
2020-12-29 14:58:25 +0800

    路由组件

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import App from './pages/App';
 import List from './pages/list';
 import Detail from './pages/detail'
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router,Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 ReactDOM.render(
-    <Router>
-      <Route exact path='/' component={App}/>
-      <Route exact path='/list:id' component={List}/>
+  <Router>
+    <Switch>
+      <Route exact path='/' component={App} />
+      <Route exact path='/list:id' component={List} />
       <Route exact path='/detail' component={Detail} />
-    </Router>,
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
 reportWebVitals();
